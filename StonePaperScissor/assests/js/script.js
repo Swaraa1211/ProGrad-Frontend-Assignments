028cb67b0fb0fba9e81e4ac7560ea021bc70ec1a
unknown <saiswaroopasaranath1211@gmail.com>
2023-05-24 15:40:29 +0530

    ADD: Game logic done.

--- StonePaperScissor/assests/js/script.js
+++ StonePaperScissor/assests/js/script.js
@@ -1,1 +1,57 @@
 //script file
+
+let [computerscore, yourscore] = [0,0];
+
+let  results = document.getElementById("gameresult");
+
+let winprediction = {
+    'rock' : {
+        'rock' : 'draw',
+        'scissor' : 'lose',
+        'paper' : 'win'
+    },
+
+    'scissor' : {
+        'rock' : 'win',
+        'scissor' : 'draw',
+        'paper' : 'lose'
+    },
+
+    'paper' : {
+        'rock' : 'lose',
+        'scissor' : 'win',
+        'paper' : 'draw'
+    }
+}
+
+function check(input){
+    var choices = ["rock", "paper", "scissor"];
+    var computer = Math.floor(Math.random()* 3);
+
+    document.getElementById("computerchoice").innerHTML = `Computer -> ${choices[computer].toUpperCase()}`;
+    document.getElementById("yourchoice").innerHTML = `You -> ${input.toUpperCase()}`
+
+    var computerchoice = choices[computer];
+
+    switch(winprediction[input][computerchoice]){
+        case 'win':
+            results.innerHTML = "You Win";
+            yourscore++;
+            break;
+
+        case 'lose':
+            results.innerHTML = "Computer Win"
+            computerscore++;
+            break;
+
+        default:
+            results.innerHTML = "Draw"
+            break;
+
+    }
+
+    document.getElementById("computerscore").innerHTML = computerscore;
+    document.getElementById("yourscore").innerHTML = yourscore;
+
+
+}
